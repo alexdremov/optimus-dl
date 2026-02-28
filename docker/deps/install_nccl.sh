e8bde8cf111cbbd465168d3a0bfa9a3f83faac47
alexdremov <dremov.me@gmail.com>
2026-02-28 09:13:25 +0100

    cache

--- docker/deps/install_nccl.sh
+++ docker/deps/install_nccl.sh
@@ -7,7 +7,13 @@ git clone https://github.com/NVIDIA/nccl.git
 cd nccl
 git checkout v2.29.3-1
 
-make -j8 src.build NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_90,code=sm_90 -gencode=arch=compute_100,code=sm_100 -gencode=arch=compute_120,code=sm_120" CUDA_HOME="/usr/local/cuda"
-make -j8 pkg.debian.build
+make -j8 src.build \
+    NVCC="ccache nvcc" \
+    CXX="ccache g++" \
+    NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_90,code=sm_90 -gencode=arch=compute_100,code=sm_100 -gencode=arch=compute_120,code=sm_120" \
+    CUDA_HOME="/usr/local/cuda"
+make -j8 pkg.debian.build \
+    NVCC="ccache nvcc" \
+    CXX="ccache g++"
 
 [ -d "/opt/nccl/build" ] && [ -d "/opt/nccl/build/include" ] && [ -d "/opt/nccl/build/lib" ]
