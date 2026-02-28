78f3a15d638d15eb20a63075fa90b47ad36612a4
alexdremov <dremov.me@gmail.com>
2026-02-28 20:43:58 +0100

    caching

--- docker/deps/install_flash.sh
+++ docker/deps/install_flash.sh
@@ -4,7 +4,20 @@ set -ex
 git clone https://github.com/HazyResearch/flash-attention
 cd flash-attention
 git checkout $FLASH_COMMIT
-uv pip install \
-    --disable-pip-version-check \
-    --no-cache-dir \
-    --no-build-isolation ./
+
+# Create a local bin directory for ccache symlinks to avoid space-separated NVCC variables
+# which break Makefile logic that uses $(firstword $(NVCC))
+rm -rf /tmp/ccache-bin || true
+mkdir -p /tmp/ccache-bin
+ln -sf /usr/bin/ccache /tmp/ccache-bin/nvcc
+ln -sf /usr/bin/ccache /tmp/ccache-bin/g++
+ln -sf /usr/bin/ccache /tmp/ccache-bin/gcc
+ln -sf /usr/bin/ccache /tmp/ccache-bin/c++
+ln -sf /usr/bin/ccache /tmp/ccache-bin/cc
+export PATH="/tmp/ccache-bin:$PATH"
+
+
+export FLASH_ATTN_CUDA_ARCHS="80;90;100;110"
+export MAX_JOBS=2
+export CUDA_HOME=/usr/local/cuda
+uv run python setup.py install
