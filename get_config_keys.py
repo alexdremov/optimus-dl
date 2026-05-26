ae0fcd8079fdf1e911c72efa4a7468dbcf0a7ced
alexdremov <dremov.me@gmail.com>
2026-05-26 16:04:14 +0200

    junk

--- get_config_keys.py
+++
@@ -1,3 +0,0 @@
-import torch
-
-print(torch._dynamo.disallow_in_graph)
--- test_liger_compile.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-print(torch.__version__)
-import torch._dynamo
-
-for k in dir(torch._dynamo.config):
-    if "compile" in k.lower():
-        print(k)
